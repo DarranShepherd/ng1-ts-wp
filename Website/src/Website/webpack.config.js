c037227a201e302c8bbf3cf291a890b743e34f89
DarranShepherd <darran@netwealth.com>
2016-11-20 20:27:03 +0000

    Config webpack to generate source map

--- Website/src/Website/webpack.config.js
+++ Website/src/Website/webpack.config.js
@@ -29,5 +29,6 @@ module.exports = {
     resolve: {
         // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
-    }
+    },
+    devtool: 'source-map'
 };
